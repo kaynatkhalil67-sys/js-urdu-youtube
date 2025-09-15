f246e964190d33a8214c2e82880f9796a6c3a7ba
kaynatkhalil67-sys <kaynatkhalil67@gmail.com>
2025-09-15 10:40:44 +0000

    datatypes done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treating all code as new version of js
+
+// alert( 3 + 3 ) we are using node js not browser, it can be used but with other way will see in future 
+console.log( 3 + 3 )
+console.log("kainat")
+// make sure the readability of code 
+let name = "kainat"
+let age = 12
+let isbool = true 
+let state; 
+let temp = null 
+// data types: 
+// num => 2 to power 53 
+// bigint
+// string => " " or ' ' 
+// boolean => true/false 
+// null => standalone value 
+// undefined 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof 'kainat')
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined 
