7928141dcea49a888684cc3997a5790bd133a1a3
kaynatkhalil67-sys <kaynatkhalil67@gmail.com>
2025-09-17 13:56:10 +0000

    summar of datatypes

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,14 @@
+//const id = symbol('123')
+//const anotherid = symbol('123')
+//console.log(id === anotherid);
+
+const heros = ["kainat" , "Arooj"] // array 
+let obj ={ 
+    name: "kainat" , 
+    age : 22
+} // obj 
+
+const Myfunction = function(){
+    console.log("hello kainat")
+} // function
+console.log(typeof Myfunction)
