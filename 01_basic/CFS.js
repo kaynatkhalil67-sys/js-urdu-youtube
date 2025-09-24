750ba4c78dad3942deeb634b921af996f639554d
kaynatkhalil67-sys <kaynatkhalil67@gmail.com>
2025-09-24 06:57:21 +0000

    cfs

---
+++ 01_basic/CFS.js
@@ -0,0 +1,43 @@
+//if
+/*
+if(true){
+
+}
+*/
+/*
+const newword= 3
+switch(newword)
+{
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break; 
+    default:
+        console.log("default case matched");
+        break;
+         
+}
+*/
+const isemail =[] // "" ==> no 
+if(isemail){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+
+const empobj ={}
+if(Object.keys(empobj).length===0)
+    {console.log('object is empty');
+}
+
+// terniary  operator syntax: condition ? true: false 
+
+const iceprice= 100
+iceprice<=80? console.log("less then 80") : console.log("more then 80");
+
